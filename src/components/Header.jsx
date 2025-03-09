c3f4952cf86b43b19318e6229369940a712e5027
Adarsh Raghu <adarshraghu500@gmail.com>
2025-03-10 00:36:25 +0530

    login page designed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import MessageModal from './MessageModal';
+import { FaBell } from "react-icons/fa";
+import { MdSpaceDashboard } from "react-icons/md";
 
 
 function Header({ dashboardName }) {
@@ -7,33 +9,25 @@ function Header({ dashboardName }) {
   const unreadNotifications = 3;
 
   return (
-    <div className="bg-white shadow-sm p-4 flex justify-between items-center">
-      <div className="text-xl font-semibold text-gray-800">{dashboardName}</div>
+    <div className="bg-white  shadow-sm p-4 flex justify-between items-center">
+      {/* <div><MdSpaceDashboard /></div> */}
+      <div className="text-xl sm:text-2xl font-semibold text-gray-800 flex justify-center items-center">
+        <p className='mx-2 text-violet-600'><MdSpaceDashboard /></p>
+        <p>{dashboardName}</p>
+        <p className='mx-2'>DaxBod</p>
+        </div>
       <div className="flex items-center space-x-4">
         <button
           onClick={() => setOpen(true)}
-          className="relative p-2 text-gray-600 hover:text-gray-900 hover:bg-gray-100 rounded-full focus:outline-none"
+          className="relative p-2 text-gray-500  hover:bg-gray-100 rounded-full focus:outline-none focus:ring-1 focus:ring-gray-200"
         >
           {/* Bell Icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-            />
-          </svg>
+          <FaBell className="text-xl "/>
 
           {/* Notification Badge */}
           {unreadNotifications > 0 && (
-            <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full px-1.5 py-0.5">
-              {unreadNotifications}
+            <span className="absolute top-1 w-2 h-2 left-6 bg-red-500 rounded-full ">
+              {/* {unreadNotifications} */}
             </span>
           )}
         </button>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [,
+//     "./src/**/*.{js,jsx,ts,tsx}", // Add paths to all your React components
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         clifford: '#da373d', // Example custom color
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
