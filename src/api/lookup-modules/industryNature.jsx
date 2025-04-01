4fe9f1940c18788111c4ad77bc484b30495880f2
joyal.minute <joyal.minute@gmail.com>
2025-04-02 02:17:25 +0530

    Lookups

---
+++ src/api/lookup-modules/industryNature.jsx
@@ -0,0 +1,7 @@
+// industryNature Apis
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+const apiUrl = import.meta.env.VITE_API_URL;
+const appVersion = import.meta.env.VITE_APP_VERSION;
+
